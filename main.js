8a7a1c581288147e58ff1f1617c8d9020bb2cae4
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-08-21 11:42:06 +0700

    updated : url

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /// --- GET DATA
 function getDatas() {
-  const url = "http://localhost:3000/siswa";
+  const url = "https://fullstack-learn-nu.vercel.app/siswa";
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function searchDatas(keyword) {
-  const url = `http://localhost:3000/siswa/search?q=${encodeURIComponent(keyword)}`;
+  const url = `https://fullstack-learn-nu.vercel.app/siswa/search?q=${encodeURIComponent(keyword)}`;
   fetch(url).then((response) => {
     response.json().then((data) => {
       const item = data.payload;
@@ -149,7 +149,7 @@ if (formSiswa) {
 
 function addData(nama, status, nik) {
   //   console.log(nik);
-  const url = "http://localhost:3000/siswa";
+  const url = "https://fullstack-learn-nu.vercel.app/siswa";
   fetch(url, {
     method: "POST",
     headers: {
@@ -203,7 +203,7 @@ if (deleteBtn) {
     if (!currentDatas.id) {
       showToast("Data is not selected!!", "#ef4444", 1500);
     } else {
-      const url = "http://localhost:3000/siswa";
+      const url = "https://fullstack-learn-nu.vercel.app/siswa";
       fetch(url, {
         method: "DELETE",
         headers: { "Content-Type": "application/json" },
@@ -250,7 +250,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function editShow(id) {
-  const url = `http://localhost:3000/siswa/${id}`;
+  const url = `https://fullstack-learn-nu.vercel.app/siswa/${id}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
@@ -298,7 +298,7 @@ if (addBtnEdit) {
       return;
     }
 
-    const url = "http://localhost:3000/siswa/update";
+    const url = "https://fullstack-learn-nu.vercel.app/siswa/update";
     fetch(url, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
